622d303da7cf6bf5c156e7b3e33bd738f8f8c6f2
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-10-20 09:40:00 +0200

    changed the fontfamily and added a custom loader

--- src/ui/Spinner.jsx
+++ src/ui/Spinner.jsx
@@ -1,5 +1,82 @@
 function Spinner() {
-  return <div className="loader"></div>;
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+      <div className="box">
+        <div className="pizza-slice slice-1">
+          <div className="border">
+            <div className="crust"></div>
+            <div className="cheese">
+              <div className="peperoni p-1"></div>
+              <div className="peperoni p-2"></div>
+              <div className="peperoni p-3"></div>
+              <div className="olive o-1"></div>
+              <div className="olive o-3"></div>
+              <div className="olive o-4"></div>
+              <div className="olive o-6"></div>
+              <div className="olive o-7"></div>
+            </div>
+          </div>
+        </div>
+        <div className="pizza-slice slice-2">
+          <div className="border">
+            <div className="crust"></div>
+            <div className="cheese">
+              <div className="peperoni p-1"></div>
+              <div className="peperoni p-2"></div>
+              <div className="peperoni p-3"></div>
+              <div className="olive o-1"></div>
+              <div className="olive o-2"></div>
+              <div className="olive o-3"></div>
+              <div className="olive o-6"></div>
+              <div className="olive o-7"></div>
+            </div>
+          </div>
+        </div>
+        <div className="pizza-slice slice-3">
+          <div className="border">
+            <div className="crust"></div>
+            <div className="cheese">
+              <div className="peperoni p-1"></div>
+              <div className="peperoni p-2"></div>
+              <div className="peperoni p-3"></div>
+              <div className="olive o-3"></div>
+              <div className="olive o-4"></div>
+              <div className="olive o-5"></div>
+              <div className="olive o-6"></div>
+            </div>
+          </div>
+        </div>
+        <div className="pizza-slice slice-4">
+          <div className="border">
+            <div className="crust"></div>
+            <div className="cheese">
+              <div className="peperoni p-1"></div>
+              <div className="peperoni p-2"></div>
+              <div className="peperoni p-3"></div>
+              <div className="olive o-1"></div>
+              <div className="olive o-2"></div>
+              <div className="olive o-3"></div>
+              <div className="olive o-4"></div>
+            </div>
+          </div>
+        </div>
+        <div className="pizza-slice slice-5">
+          <div className="border">
+            <div className="crust"></div>
+            <div className="cheese">
+              <div className="peperoni p-1"></div>
+              <div className="peperoni p-2"></div>
+              <div className="peperoni p-3"></div>
+              <div className="olive o-1"></div>
+              <div className="olive o-2"></div>
+              <div className="olive o-6"></div>
+              <div className="olive o-7"> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Spinner;
