6b77d95ace80754f11448ab85ea9da5a213c6555
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-10-20 09:59:30 +0200

    styled the menu page

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to }) {
-  const className =
-    "inline-block rounded-full bg-btn px-4 py-3 font-semibold uppercase tracking-wide text-white transition-colors duration-300 hover:bg-btnHover focus:bg-btnHover focus:outline-none focus:ring focus:ring-btn focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-slate-600 sm:px-6 sm:py-4";
+function Button({ children, disabled, to, type }) {
+  const base =
+    "inline-block rounded-full bg-btn font-semibold uppercase tracking-wide text-white transition-colors duration-300 hover:bg-btnHover focus:bg-btnHover focus:outline-none focus:ring focus:ring-btn focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-slate-600 sm:px-6 sm:py-4";
+
+  const styles = {
+    primary: base + " px-4 py-3 md:px-6 md:py-4",
+    small: base + " py-2 px-4 md:px-5 md:py-2.5 text-xs",
+  };
 
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
 
   return (
-    <button className={className} disabled={disabled}>
+    <button className={styles[type]} disabled={disabled}>
       {children}
     </button>
   );
